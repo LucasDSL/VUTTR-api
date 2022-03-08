42973410c1ae84cbe1600ff7e8f7960c9ceba587
LucasDSL <lucasdslima2002@gmail.com>
2022-03-07 22:58:07 -0300

    latest dependencies and configuring imports on app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ToolsModule } from './tools/tools.module';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { Tool } from './tools/entities/tool.entity';
+import { Tag } from './tools/entities/tag.entity';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    ToolsModule,
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: Number.parseInt(process.env.DB_PORT),
+      host: process.env.DB_HOST,
+      synchronize: true,
+      entities: [User, Tool, Tag],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
